4a6bfe162ed344265c711d36398535ff21d7f846
Enno Runne <458526+ennru@users.noreply.github.com>
2019-10-14 17:24:39 +0200

    Scala 2.12.10 and 2.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 
 object Dependencies {
   val Scala211 = "2.11.12"
-  val Scala212 = "2.12.8"
-  val Scala213 = "2.13.0-M5"
+  val Scala212 = "2.12.10"
+  val Scala213 = "2.13.1"
 
   val AkkaVersion = "2.5.21"
   val AlpakkaCouchbaseVersion = "1.0.1"
